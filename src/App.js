30f77691c8a861bd4900c4221c28af5b9a4372b7
khuzaima yaseen <khuzaimayaseen@gmail.com>
2020-09-12 20:26:56 +0500

    Fix all problems in tables.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Login from "./Links/Login/Login.jsx"
-import Home1 from "./Links/Home/index.js"
-import {BrowserRouter,Link,Switch,Route} from 'react-router-dom';
-import CantLogin from './Links/CantLogin/CantLogin.jsx';
-import Home from './Links/Dashboard/Dashboard.jsx';
-import Setting from './Links/Settings/Setting.jsx';
-import Support from './Links/Support/Support.jsx';
+import React from "react";
+import Login from "./Links/Login/Login.jsx";
+import Home1 from "./Links/Home/index.js";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import CantLogin from "./Links/CantLogin/CantLogin.jsx";
+import Home from "./Links/Dashboard/Dashboard.jsx";
+import Setting from "./Links/Settings/Setting.jsx";
+import Support from "./Links/Support/Support.jsx";
 import Contact from "./Links/Support/Contact.jsx";
 import Notification from "./Links/Notification/index.js";
 import LatestAlerts from "./Links/Notification/LatestAlerts.jsx";
@@ -13,90 +13,103 @@ import ServiceUpdates from "./Links/Notification/ServiceUpdates.js";
 import SupportUpdates from "./Links/Notification/SupportUpdates.js";
 import Exposed from "./Links/Exposed/index.js";
 import Monitor from "./Links/Monitored/index.js";
-import { MuiThemeProvider } from '@material-ui/core';
-import theme from './theme';
-import BlackMarket from './Links/Exposed/blackmarket.js';
-import Discussion from './Links/Exposed/discussions.js';
-import Sensitive from './Links/Exposed/sensitiveinformation.js';
-import Financial from './Links/Exposed/financial.js';
-import ExposedCredentials from './Links/Exposed/exposedcredentials.js';
-import PersonalInformation from './Links/Exposed/personalinformation.js';
-import HackerGroupTargeting from './Links/Exposed/hackergrouptargeting.js';
-import AttacksAndCompromises from './Links/Exposed/attacksandcompromises.js';
-import UnderAnalysis from './Links/Exposed/underanalysis.js';
-import Remediation from './Links/Exposed/remediation.js';
-import Contact1 from './Links/Contact/Contact.js';
+import { MuiThemeProvider } from "@material-ui/core";
+import theme from "./theme";
+import BlackMarket from "./Links/Exposed/blackmarket.js";
+import Discussion from "./Links/Exposed/discussions.js";
+import Sensitive from "./Links/Exposed/sensitiveinformation.js";
+import Financial from "./Links/Exposed/financial.js";
+import ExposedCredentials from "./Links/Exposed/exposedcredentials.js";
+import PersonalInformation from "./Links/Exposed/personalinformation.js";
+import HackerGroupTargeting from "./Links/Exposed/hackergrouptargeting.js";
+import AttacksAndCompromises from "./Links/Exposed/attacksandcompromises.js";
+import UnderAnalysis from "./Links/Exposed/underanalysis.js";
+import Remediation from "./Links/Exposed/remediation.js";
+import Contact1 from "./Links/Contact/Contact.js";
 import Discover from "./Links/Discover/index.js";
-import MainDashboard from './Links/MainDashboard/index.js';
+import MainDashboard from "./Links/MainDashboard/index.js";
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-    <div className="App">
-      <head>
-      <link
+      <div className="App">
+        <head>
+          <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-      </head>
-       <BrowserRouter>
-        
-        
-        
-        <Route path="/" component={Login} exact />
-        <Route path="/CantLogin" component={CantLogin} exact/>
-        <Route path="/Home" component={Home1} exact/>
-        <Route path="/MDashBoard" component={MainDashboard} exact/>
-        <Route path="/DashBoard" component={Home} exact/>
-        <Route path="/Discover" component={Discover} exact/>
-        <Route path="/Settings" component={Setting} exact/>
-        <Route path="/Support" component={Support} exact/>
-        <Route path="/Notification" component={Notification} exact/>
-        <Route path="/LatestAlerts" component={LatestAlerts} exact/>
-        <Route path="/ServiceUpdates" component={ServiceUpdates} exact/>
-        <Route path="/SupportUpdates" component={SupportUpdates} exact/>
-        <Route path="/Contact" component={Contact} exact/>
-        <Route path="/ExposedInformation" component={Exposed} exact/>
-        <Route path="/BlackMarket" component={BlackMarket} exact/>
-        <Route path="/Discussion" component={Discussion} exact/>
-        <Route path="/SensitiveInformation" component={Sensitive} exact/>
-        <Route path="/Financial" component={Financial} exact/>
-        <Route path="/Monitored" component={Monitor} exact/>
-        <Route path="/ExposedCredentials" component={ExposedCredentials} exact/>
-        <Route path="/PersonalInformation" component={PersonalInformation} exact/>
-        <Route path="/HackerGroupTargeting" component={HackerGroupTargeting} exact/>
-        <Route path="/AttackandCompromises" component={AttacksAndCompromises}  exact/>
-        <Route path="/UnderAnalysis" component={UnderAnalysis}  exact/>
-        <Route path="/Remediation" component={Remediation} exact/>
-        <Route path="/Contactus" component={Contact1} exact/>
-        
-      </BrowserRouter>
-      <style jsx global>{`
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          ></meta>
+        </head>
+        <BrowserRouter>
+          <Route path="/" component={Login} exact />
+          <Route path="/CantLogin" component={CantLogin} exact />
+          <Route path="/Home" component={Home1} exact />
+          <Route path="/MDashBoard" component={MainDashboard} exact />
+          <Route path="/DashBoard" component={Home} exact />
+          <Route path="/Discover" component={Discover} exact />
+          <Route path="/Settings" component={Setting} exact />
+          <Route path="/Support" component={Support} exact />
+          <Route path="/Notification" component={Notification} exact />
+          <Route path="/LatestAlerts" component={LatestAlerts} exact />
+          <Route path="/ServiceUpdates" component={ServiceUpdates} exact />
+          <Route path="/SupportUpdates" component={SupportUpdates} exact />
+          <Route path="/Contact" component={Contact} exact />
+          <Route path="/ExposedInformation" component={Exposed} exact />
+          <Route path="/BlackMarket" component={BlackMarket} exact />
+          <Route path="/Discussion" component={Discussion} exact />
+          <Route path="/SensitiveInformation" component={Sensitive} exact />
+          <Route path="/Financial" component={Financial} exact />
+          <Route path="/Monitored" component={Monitor} exact />
+          <Route
+            path="/ExposedCredentials"
+            component={ExposedCredentials}
+            exact
+          />
+          <Route
+            path="/PersonalInformation"
+            component={PersonalInformation}
+            exact
+          />
+          <Route
+            path="/HackerGroupTargeting"
+            component={HackerGroupTargeting}
+            exact
+          />
+          <Route
+            path="/AttackandCompromises"
+            component={AttacksAndCompromises}
+            exact
+          />
+          <Route path="/UnderAnalysis" component={UnderAnalysis} exact />
+          <Route path="/Remediation" component={Remediation} exact />
+          <Route path="/Contactus" component={Contact1} exact />
+        </BrowserRouter>
+        <style jsx global>{`
           html,
           body {
-            width:100%;
-            height:100%;
+            width: 100%;
+            height: 100%;
             padding: 0;
             margin: 0;
             font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
               Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
               sans-serif;
           }
-          a{
-              text-decoration:none
+          a {
+            text-decoration: none;
           }
-          .percent-text{
+          .percent-text {
             text-anchor: middle;
-            font-family: 'Roboto', sans-serif;
-            
+            font-family: "Roboto", sans-serif;
           }
           * {
             box-sizing: border-box;
           }
-        `}</style>                    
-      
-    </div>
+        `}</style>
+      </div>
     </MuiThemeProvider>
   );
 }
--- src/components/remediation/FContent.jsx
+++ src/components/remediation/FContent.jsx
@@ -1,234 +1,231 @@
-import React,{useEffect, useState} from 'react';
-import {Grid,Typography,Card,Paper,CardHeader,Divider,Button, IconButton} from "@material-ui/core";
-import { makeStyles,withStyles } from '@material-ui/core/styles';
+import React, { useEffect, useState } from "react";
+import {
+  Grid,
+  Typography,
+  Card,
+  Paper,
+  CardHeader,
+  Divider,
+  Button,
+  IconButton,
+} from "@material-ui/core";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import refresh from "../../Links/images/refresh.png";
 import warning from "../../Links/images/warning.png";
 import active from "../../Links/images/active.png";
 import new1 from "../../Links/images/new.png";
-import activesource from "../../Links/images/activesource.png"
+import activesource from "../../Links/images/activesource.png";
 import Progressbar from "./progressbar";
 import BarGroup from "./BarGroup.jsx";
 import Piechart from "./Piechart.jsx";
 import drop from "../../Links/images/drop.png";
 import Dropup from "./Dropup.jsx";
 import Dropdown from "./Dropdown.jsx";
-import GetAppIcon from '@material-ui/icons/GetApp';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import Tooltip from '@material-ui/core/Tooltip';
-import A from '../Drawer/Dashboard/Tabs';
-import Highlight from '../Drawer/Dashboard/Highlight.js'
-import DDContent from './DDContent.jsx';
-import DDContent1 from './DDContent1.js';
+import GetAppIcon from "@material-ui/icons/GetApp";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import Tooltip from "@material-ui/core/Tooltip";
+import A from "../Drawer/Dashboard/Tabs";
+import Highlight from "../Drawer/Dashboard/Highlight.js";
+import DDContent from "./DDContent.jsx";
+import DDContent1 from "./DDContent1.js";
 const LightTooltip = withStyles((theme) => ({
-    tooltip: {
-      backgroundColor: theme.palette.common.white,
-      color: 'rgba(0, 0, 0, 0.87)',
-      boxShadow: theme.shadows[1],
-      fontSize: 11,
-    },
-  }))(Tooltip);
+  tooltip: {
+    backgroundColor: theme.palette.common.white,
+    color: "rgba(0, 0, 0, 0.87)",
+    boxShadow: theme.shadows[1],
+    fontSize: 11,
+  },
+}))(Tooltip);
 const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-      
-    },
-    button:{
-        display:"flex",alignItems:"center",width:"100%",height:"44%",border: "0.6px solid #000000",borderRadius: "2px",fontStyle: "normal",
-        fontSize:"10px",                                            
-                                                    
-        minWidth:"100px",
-        color: "rgba(0, 0, 0, 0.6)"
-    },
-    h:{
-        fontStyle: "normal",
-        fontWeight: "normal",
-        fontSize: "27.9411px"
-    },
-    HeaderFont:{
-        fontFamily: 'Roboto',
-        fontSize: "16.9411px" ,
-          fontWeight:"bold",
-          fontStyle:"normal"
-      },
-      progressfont:{
-        
-        fontStyle: "normal",
-        fontWeight: "500",
-        fontSize: "14px",
+  root: {
+    flexGrow: 1,
+  },
+  button: {
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    height: "44%",
+    border: "0.6px solid #000000",
+    borderRadius: "2px",
+    fontStyle: "normal",
+    fontSize: "10px",
 
-        letterSpacing: "-0.0864285px",
-        
-        color: "#506883"
-      },
-      paper: {
-          
-         
-        background: "#FFFFFF",
-        boxShadow: "0px 0px 35px rgba(181, 181, 195, 0.15)",
-        borderRadius: "6px", 
-        padding: theme.spacing(2),
-        height: "auto",
-        
-        width:"12vw",
-        minWidth:"190px",
-        display:"flex",flexDirection:"column",alignItems:"center"
+    minWidth: "100px",
+    color: "rgba(0, 0, 0, 0.6)",
+  },
+  h: {
+    fontStyle: "normal",
+    fontWeight: "normal",
+    fontSize: "27.9411px",
+  },
+  HeaderFont: {
+    fontFamily: "Roboto",
+    fontSize: "16.9411px",
+    fontWeight: "bold",
+    fontStyle: "normal",
+  },
+  progressfont: {
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: "14px",
 
-      },
-    but:{
-        display:"flex",
-        
-        width: "159px",
-        height: "34px",
-        marginTop:"14px",
-        background: "#464E5F",
-        border: "1px solid #000000",
-        boxSizing: "border-box",
-        borderRadius: "4px"
+    letterSpacing: "-0.0864285px",
 
-    },
-    dot:{
-        marginTop:"10px",
-        display:"flex",
-        alignItems:"center",
-        justifyContent:"center",
-        height: "60px",
-        width: "60px",
-        background:"black",
-        borderRadius: "50%",
-        
-    },
-    fontin:{
-        paddingTop:'20px',
-        fontStyle: "normal",
-        fontWeight: "500",
-        fontSize: "16px",
-        lineHeight: "18px",
-        /* identical to box height, or 112% */
-        
-        
-        color: "#B5B5C3"
-    },
-    f1:{
-       width:"100%",
-       boxShadow: "0px 0px 19.9579px rgba(56, 71, 109, 0.03)",
-        borderRadius: "11.9747px",
-        
-        height: "422.11px"
-    },
-    f4:{
-        width:"28vw",
-        minWidth:"300px",
-        boxShadow: "0px 0px 19.9579px rgba(56, 71, 109, 0.03)",
-        borderRadius: "11.9747px",
-        height: "380px"
-        
-    },
-    f5:{
-        width:"100%",
-        boxShadow: "0px 0px 19.9579px rgba(56, 71, 109, 0.03)",
-         borderRadius: "11.9747px",
-         
-         paddingBottom:"18px",
-         height:"447px"
-    },
-    f2:{
-        background: "#FFFFFF",
-        boxShadow: "0px 0px 35px rgba(181, 181, 195, 0.15)",
-        borderRadius: "6px", 
-        
-        height: "405px",
-        
-     },
-     f3:{
-        width:"33.3vw",
-        minWidth:"400px",
-        boxShadow: "0px 0px 19.9579px rgba(56, 71, 109, 0.03)",
-        borderRadius: "11.9747px",
-        height: "380px"
-     },
-    control: {
-      padding: theme.spacing(2),
-    },
-  }));
-export default function FContent(){
-    const classes = useStyles();
-    const handleClickOpen2=()=>{
-        
-    }
-    const [index1,setindex]=React.useState({index:0});
+    color: "#506883",
+  },
+  paper: {
+    background: "#FFFFFF",
+    boxShadow: "0px 0px 35px rgba(181, 181, 195, 0.15)",
+    borderRadius: "6px",
+    padding: theme.spacing(2),
+    height: "auto",
 
-    const [value, setValue] = React.useState('2');
+    width: "12vw",
+    minWidth: "190px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  but: {
+    display: "flex",
 
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-    };
-    const handle1=(index1)=>{
-        console.log('index',index1)
-        setindex({index:index1})
-    }
-    const [count,setcount]=useState();
-    const [view,setview]=useState(true);
-    const [dropc,setdrop]=useState(true);
-    const [filter,setfilter]=useState(true);
-    const handleview=()=>{
-            setview(true)
-    }
-    const handleview1=()=>{
-        setview(false)
-}
-    const handledrop=()=>{
-            setdrop(true)
-    }
-    const handledrop1=()=>{
-        setdrop(false)
-    }
-    const calculate = ()=>{
-        let char,calculate,x;
-        char=(70/300)*100;
-        console.log(char,"char")
-        x=Math.round(char)
-        calculate=String(x).concat("%")
-        
-        setcount(calculate)
-        
+    width: "159px",
+    height: "34px",
+    marginTop: "14px",
+    background: "#464E5F",
+    border: "1px solid #000000",
+    boxSizing: "border-box",
+    borderRadius: "4px",
+  },
+  dot: {
+    marginTop: "10px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "60px",
+    width: "60px",
+    background: "black",
+    borderRadius: "50%",
+  },
+  fontin: {
+    paddingTop: "20px",
+    fontStyle: "normal",
+    fontWeight: "500",
+    fontSize: "16px",
+    lineHeight: "18px",
+    /* identical to box height, or 112% */
 
-    }
-    const changefilter=()=>{
-        setfilter(!filter)
-    }
-    useEffect(() => {
-        calculate();
-      });
-    return(
-        <div >
-            <Grid container>
-                <Grid item xs={12}>
-                    <A.Tabs value={index1.index} onChange={(_,index1)=>handle1(index1)}>
-                        <A.Tab title="Actions" />
-                        <A.Tab title="Log"/>
-                        
-                    </A.Tabs>
+    color: "#B5B5C3",
+  },
+  f1: {
+    width: "100%",
+    boxShadow: "0px 0px 19.9579px rgba(56, 71, 109, 0.03)",
+    borderRadius: "11.9747px",
 
-                    
+    height: "422.11px",
+  },
+  f4: {
+    width: "28vw",
+    minWidth: "300px",
+    boxShadow: "0px 0px 19.9579px rgba(56, 71, 109, 0.03)",
+    borderRadius: "11.9747px",
+    height: "380px",
+  },
+  f5: {
+    width: "100%",
+    boxShadow: "0px 0px 19.9579px rgba(56, 71, 109, 0.03)",
+    borderRadius: "11.9747px",
 
-                    <A.TabContent value={index1.index} index={0}>
-                    <Grid item>
-                        <DDContent/>
-                    </Grid>
-                    </A.TabContent>
-                    <A.TabContent value={index1.index}  index={1}>
-                    <Grid item >
-                        <DDContent1/>
-                    </Grid>
-                    </A.TabContent>
-                    
+    paddingBottom: "18px",
+    height: "447px",
+  },
+  f2: {
+    background: "#FFFFFF",
+    boxShadow: "0px 0px 35px rgba(181, 181, 195, 0.15)",
+    borderRadius: "6px",
 
+    height: "405px",
+  },
+  f3: {
+    width: "33.3vw",
+    minWidth: "400px",
+    boxShadow: "0px 0px 19.9579px rgba(56, 71, 109, 0.03)",
+    borderRadius: "11.9747px",
+    height: "380px",
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+export default function FContent() {
+  const classes = useStyles();
+  const handleClickOpen2 = () => {};
+  const [index1, setindex] = React.useState({ index: 0 });
 
+  const [value, setValue] = React.useState("2");
 
-                </Grid>
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const handle1 = (index1) => {
+    console.log("index", index1);
+    setindex({ index: index1 });
+  };
+  const [count, setcount] = useState();
+  const [view, setview] = useState(true);
+  const [dropc, setdrop] = useState(true);
+  const [filter, setfilter] = useState(true);
+  const handleview = () => {
+    setview(true);
+  };
+  const handleview1 = () => {
+    setview(false);
+  };
+  const handledrop = () => {
+    setdrop(true);
+  };
+  const handledrop1 = () => {
+    setdrop(false);
+  };
+  const calculate = () => {
+    let char, calculate, x;
+    char = (70 / 300) * 100;
+    console.log(char, "char");
+    x = Math.round(char);
+    calculate = String(x).concat("%");
+
+    setcount(calculate);
+  };
+  const changefilter = () => {
+    setfilter(!filter);
+  };
+  useEffect(() => {
+    calculate();
+  });
+  return (
+    <div>
+      <Grid container>
+        <Grid item xs={12}>
+          <A.Tabs
+            value={index1.index}
+            onChange={(_, index1) => handle1(index1)}
+          >
+            <A.Tab title="Actions" />
+            <A.Tab title="Log" />
+          </A.Tabs>
+
+          <A.TabContent value={index1.index} index={0}>
+            <Grid item>
+              <DDContent />
+            </Grid>
+          </A.TabContent>
+          <A.TabContent value={index1.index} index={1}>
+            <Grid item>
+              <DDContent1 />
+            </Grid>
+          </A.TabContent>
         </Grid>
-
-
-        </div>
-    )
+      </Grid>
+    </div>
+  );
 }
